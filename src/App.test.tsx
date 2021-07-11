f6c934b44e4ddc73b943b7046ae41cb83283ece7
Bruno Lima <brunolima.qi@gmail.com>
2021-07-11 18:50:54 -0300

    remover testes

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Map from './components/map'
-
-test('renders learn react link', () => {
-  render(<Map />);
-  const linkElement = screen.getByText(/Play/i);
-  expect(linkElement).toBeInTheDocument();
-});
