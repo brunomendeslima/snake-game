fc87591ac33a48a8ac1ac5c800b8ba9ef4f05cc3
Bruno Lima <brunolima.qi@gmail.com>
2021-07-11 18:30:30 -0300

    remover testes

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Map from './components/map'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Map />);
+  const linkElement = screen.getByText(/Play/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import "../../App.css"
 import Fruit from '../fruit'
 import OldMonitor from '../oldmonitor'
